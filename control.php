7e2f38a4029dcdde3a8ac7796ac03628c965e807
Igor Padoim <igorpadoim@gmail.com>
2020-12-14 09:35:24 -0300

    Set plugin header

---
+++ control.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:         Control
+ * Plugin URI:          https://padoim.com/
+ * Description:         Maneje os pacientes de grupos, usuários de insulina, gestantes e mais com esse plugin.
+ * Version:             0.1.0
+ * Requires at least:   5.2
+ * Requires PHP:        7.2
+ * Author:              Igor Padoim
+ * Author URI:          https://padoim.com/
+ * License:             Unlicense
+ * License URI:         https://unlicense.org/
+ * Text Domain:         nereare-control
+ */
+?>
